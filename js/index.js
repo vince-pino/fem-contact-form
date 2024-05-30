77a9b53a98d6843c81722dd56a7a9f1f02b2624d
Vince Cedie Pino <vincecediepino@gmail.com>
2024-05-30 20:47:06 +0700

    add styles in index.js file

--- js/index.js
+++ js/index.js
@@ -1,8 +1,36 @@
+const firstNameInput = document.querySelector('.first-name input');
+const lastNameInput = document.querySelector('.last-name input');
+const emailInput = document.querySelector('.email-section input');
+
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  return regex.test(email);
+}
+
 const options = document.querySelectorAll('.radio-button-container');
 
 options.forEach((option) => {
   option.addEventListener('click', () => {
-    option.querySelector('input').checked = true;
+    const radioInput = option.querySelector('input');
+    const isChecked = radioInput.checked;
+
+    if (!isChecked) {
+      options.forEach((option) => {
+        const input = option.querySelector('input');
+        const img = option.querySelector('img');
+        
+        if (input === radioInput) {
+          input.checked = true;
+          img.style.display = 'block';
+        }
+        else {
+          img.style.display = 'none';
+          input.checked = false;
+        }
+
+      })
+    }
   });
 });
 
@@ -10,12 +38,30 @@ const label = document.querySelector('.checkbox-section label');
 const checkbox = label.querySelector('input[type="checkbox"]');
 const img = label.querySelector('img');
 
-console.log(label)
-
 checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
     img.style.display = 'block';
   } else {
     img.style.display = 'none';
   }
+});
+
+const submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click', () => {
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
+  const email = emailInput.value;
+  const isEmailValid = validateEmail(email);
+
+  if (firstName === '' ) {
+    document.querySelector('.first-name input').classList.add('error-input');
+  } 
+  if (lastName === '') {  
+    document.querySelector('.last-name input').classList.add('error-input');
+  }
+  if (!isEmailValid) {
+    document.querySelector('.email-section input').classList.add('error-input');
+  }
+  
 });
