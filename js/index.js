063812c4d2492f3e59e4b3ecc0897e27be7004ce
Vince Cedie Pino <vincecediepino@gmail.com>
2024-06-21 16:22:40 +0700

    fix custom radio buttons

--- js/index.js
+++ js/index.js
@@ -9,28 +9,28 @@ function validateEmail(email) {
 }
 
 const options = document.querySelectorAll('.radio-button-container');
+let radioVal = 0;
 
 options.forEach((option) => {
   option.addEventListener('click', () => {
-    const radioInput = option.querySelector('input');
-    const isChecked = radioInput.checked;
+    const img = option.querySelector('img');
+    const img1 = document.querySelector('#img1');
+    const img2 = document.querySelector('#img2');
 
-    if (!isChecked) {
-      options.forEach((option) => {
-        const input = option.querySelector('input');
-        const img = option.querySelector('img');
-        
-        if (input === radioInput) {
-          input.checked = true;
-          img.style.display = 'block';
-        }
-        else {
-          img.style.display = 'none';
-          input.checked = false;
-        }
-
-      })
+    if (!img1.classList.contains("hidden") && img2.classList.contains("hidden")) {
+      img1.classList.add("hidden");
+      img2.classList.remove("hidden");
+      radioVal = 1;
+    } 
+    else if (!img2.classList.contains('hidden') && img1.classList.contains('hidden')) {
+      img2.classList.add('hidden');
+      img1.classList.remove('hidden');
+      radioVal = 2;
     }
+    else {
+      img.classList.remove("hidden");
+    }
+
   });
 });
 
@@ -53,15 +53,46 @@ submitBtn.addEventListener('click', () => {
   const lastName = lastNameInput.value;
   const email = emailInput.value;
   const isEmailValid = validateEmail(email);
+  const emailError = document.querySelectorAll('.email-section p');
+  const img1 = document.querySelector('#img1');
+  const img2 = document.querySelector('#img2');
 
   if (firstName === '' ) {
     document.querySelector('.first-name input').classList.add('error-input');
-  } 
+    document.querySelector('.first-name p').style.display = 'block';
+  }
+  else {
+    document.querySelector('.first-name input').classList.remove('error-input');
+    document.querySelector('.first-name p').style.display = 'none';
+  }
   if (lastName === '') {  
     document.querySelector('.last-name input').classList.add('error-input');
+    document.querySelector('.last-name p').style.display = 'block';
   }
-  if (!isEmailValid) {
+  else {
+    document.querySelector('.last-name input').classList.remove('error-input');
+    document.querySelector('.last-name p').style.display = 'none';
+  }
+  if (email === '') {
     document.querySelector('.email-section input').classList.add('error-input');
+    emailError[1].style.display = 'block';
+    emailError[0].style.display = 'none';
   }
-  
+  else if (!isEmailValid) {
+    document.querySelector('.email-section input').classList.add('error-input');
+    emailError[0].style.display = 'block';
+    emailError[1].style.display = 'none';
+  }
+  else {
+    document.querySelector('.email-section input').classList.remove('error-input');
+    emailError[0].style.display = 'none';
+    emailError[1].style.display = 'none';
+  }
+  // fix this
+  if (img1.classList.contains('hidden') && img2.classList.contains('hidden')) {
+    document.querySelector('.option-section p').style.display = 'block';
+  }
+  else {
+    document.querySelector('.option-section p').style.display = 'none';
+  }
 });
